ac204bb797613c0d7dd2aa9298716120579a12fe
Martin Karry <martin@karry.de>
2022-03-06 15:59:29 +0100

    Fixed validation process

--- src/model/process/process.functions.ts
+++ src/model/process/process.functions.ts
@@ -1,34 +1,26 @@
 import { ProcessMissingEntryTarget, ProcessMissingTarget } from "./process.errors";
 import { State } from "./State.interface";
 import { Process } from "./Process.interface";
-import { StateTransition } from "./StateTransition.interface";
 
 export const validateProcess = async (process: Process): Promise<true> => {
     const validKeys: string[] = Object.keys(process).filter((key: string) => key.startsWith("state"));
 
     if (!validKeys.includes(process.entryProcess)) {
-        throw new ProcessMissingEntryTarget(`Entry process "${process.entryProcess}" doesn't exist`);
+        throw new ProcessMissingEntryTarget(`Missing state for Entry: "${process.entryProcess}"`);
     }
 
-    const valid: boolean[] = validKeys.map((key: string) => {
-        const state: State = process[key as `state${string}`];
-        if (!state.transitions) {
-            return true;
-        }
+    const invalidKeys: string[] = getInvalidKeys(validKeys, validKeys.map(key => process[key as `state${string}`]));
 
-        if (state.transitions.length === 0) {
-            return true;
-        }
-
-        return state
-            .transitions
-            .map((transition: StateTransition) => transition.target)
-            .every((target: string) => validKeys.includes(target));
-    })
-
-    if (valid.includes(false)) {
-        throw new ProcessMissingTarget(`Wrong target`);
+    if (invalidKeys.length > 0) {
+        throw new ProcessMissingTarget(`Missing targeted states: ${invalidKeys.join(", ")}`)
     }
 
     return true;
+}
+
+const getInvalidKeys = (validKeys: string[], states: State[]): string[] => {
+    return states
+        .map(state => state.transitions?.map(transition => transition.target) || [])
+        .flat()
+        .filter(taget => !validKeys.includes(taget))
 }
