c9f92cf8e5860099d5357a289ad88a6a6469b689
Martin Karry <martin@karry.de>
2022-03-07 20:02:28 +0100

    renamed function

--- src/hessian.ts
+++ src/hessian.ts
@@ -2,7 +2,7 @@ export { Subject } from "./model/Subject.interface"
 
 export { ContentTypeDefinition } from "./model/ContentTypeDefinition.interface"
 
-export { validateProcess } from "./model/process/process.functions"
+export { validateProcess } from "./model/process/validateProcess.function"
 export { ProcessMissingEntryTarget, ProcessMissingTarget } from "./model/process/process.errors"
 export { Process } from "./model/process/Process.interface"
 export { State } from "./model/process/State.interface"
