3233cf019d9256d079f8685db1de24e1d28f3d41
Martin Karry <martin@karry.de>
2022-03-15 20:07:53 +0100

    Added new Content models

--- src/model/content/Content.interface.ts
+++ src/model/content/Content.interface.ts
@@ -1,3 +1,13 @@
-export interface Content {
-    
+import { FileContent } from "./FileContent.interface";
+import { ObjectContent } from "./ObjectContent.interface";
+
+export interface Content<T extends ObjectContent | FileContent> {
+    id?: string;
+    name?: string;
+    payload: T;
+    version?: number;
+    history: T[];
+    taxonomies?: {
+        [taxonomy: string]: string[]
+    }[]
 }
---
+++ src/model/content/ObjectContent.interface.ts
@@ -0,0 +1,3 @@
+export interface ObjectContent {
+    type: "OBJECT"
+}
